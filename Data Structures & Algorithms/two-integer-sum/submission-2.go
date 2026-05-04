8143cd80a10982df118815e996bd8d88954fa60d
Artem Chen <art.chen.v@gmail.com>
2026-05-04 19:22:44 +0900

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.go
@@ -0,0 +1,12 @@
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	
+	for i, n := range nums {
+		if v, ok := m[target-n]; ok {
+			return []int{v, i}
+		}
+		m[n] = i
+	}
+
+	return []int{}
+}
