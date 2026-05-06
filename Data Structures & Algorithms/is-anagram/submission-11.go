3a10a45a7eca002f44322b6e81916891e34d753b
Artem Chen <art.chen.v@gmail.com>
2026-05-06 17:17:29 +0900

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.go
@@ -0,0 +1,20 @@
+func isAnagram(s string, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	sMap := make(map[rune]int)
+
+	for _, r := range s {
+		sMap[r]++
+	}
+
+	for _, r := range t {
+		count, ok := sMap[r]
+		if !ok || count == 0 {
+			return false
+		}
+		sMap[r]--
+	}
+
+	return true
+}
