fb854c509dd3f352720a52bc16fccfdedd18fc1d
Artem Chen <art.chen.v@gmail.com>
2026-05-07 17:25:29 +0900

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.go
@@ -0,0 +1,17 @@
+func lengthOfLongestSubstring(s string) int {
+	set := make(map[byte]struct{})
+	longest := 0
+	l := 0
+	for r := 0; r < len(s); r++ {
+		for {
+			if _, ok := set[s[r]]; !ok {
+				break
+			}
+			delete(set, s[l])
+			l++
+		}
+		longest = max(longest, r-l+1)
+		set[s[r]] = struct{}{}
+	}
+	return longest
+}
