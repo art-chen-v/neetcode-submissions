df054831b0ed6dd646f06aea8ae601ba68a7b99c
Artem Chen <art.chen.v@gmail.com>
2026-05-09 00:15:29 +0900

    Add: longest-repeating-substring-with-replacement - submission-7

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-7.go
@@ -0,0 +1,26 @@
+func characterReplacement(s string, k int) int {
+	longest := 0
+	counter := make(map[byte]int)
+
+	l := 0
+	for r := 0; r < len(s); r++ {
+		counter[s[r]]++
+		for {
+			if (r-l+1) - maxLetterCount(counter) <= k {
+				break
+			}
+			counter[s[l]]--
+			l++
+		}
+		longest = max(longest, r-l+1)
+	}
+	return longest
+}
+
+func maxLetterCount(counter map[byte]int) int {
+	longest := 0
+	for _, v := range counter {
+		longest = max(longest, v)
+	}
+	return longest
+}
