9c5c20d01fb205445a6c1b810f9af16a3911a43e
Artem Chen <art.chen.v@gmail.com>
2026-05-16 19:25:09 +0900

    Add: three-integer-sum - submission-9

---
+++ Data Structures & Algorithms/three-integer-sum/submission-9.go
@@ -0,0 +1,32 @@
+import "slices"
+
+func threeSum(nums []int) [][]int {
+	slices.Sort(nums)
+	res := make([][]int, 0)
+	for i, a := range nums {
+		if a > 0 {
+			break
+		}
+		if i > 0 && a == nums[i-1] {
+			continue
+		}
+		l := i + 1
+		r := len(nums) - 1
+		for l < r {
+			sum := a + nums[l] + nums[r]
+			if sum < 0 {
+				l++
+			} else if sum > 0 {
+				r--
+			} else {
+				res = append(res, []int{a, nums[l], nums[r]})
+				l++
+				r--
+				for l < r && nums[l] == nums[l-1] {
+					l++
+				}
+			}
+		}
+	}
+	return res
+}
