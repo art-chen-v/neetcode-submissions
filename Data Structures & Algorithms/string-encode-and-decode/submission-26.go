d0289fbc9088b5dc4f5f9e62829efd6ef49f691c
Artem Chen <art.chen.v@gmail.com>
2026-05-17 15:11:05 +0900

    Add: string-encode-and-decode - submission-26

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-26.go
@@ -0,0 +1,27 @@
+type Solution struct{}
+
+func (s *Solution) Encode(strs []string) string {
+	var sb strings.Builder
+	for _, s := range strs {
+		sb.WriteString(strconv.Itoa(len(s)))
+		sb.WriteString("#")
+		sb.WriteString(s)
+	}
+	return sb.String()
+}
+
+func (s *Solution) Decode(encoded string) []string {
+	res := make([]string, 0)
+	l, r := 0, 0
+	for r < len(encoded) {
+		for encoded[r] != '#' {
+			r++
+		}
+		strLen, _ := strconv.Atoi(encoded[l:r])
+		l = r + 1
+		res = append(res, encoded[l:l+strLen])
+		l = l + strLen
+		r = l
+	}
+	return res
+}
