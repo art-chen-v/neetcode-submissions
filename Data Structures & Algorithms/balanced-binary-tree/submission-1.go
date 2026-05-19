1d030fc46c12312ef73a6e542f4c88be430296fc
Artem Chen <art.chen.v@gmail.com>
2026-05-20 01:13:39 +0900

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.go
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func isBalanced(root *TreeNode) bool {
+    var dfs func(root *TreeNode) (int, bool)
+	dfs = func(root *TreeNode) (int, bool) {
+		if root == nil {
+			return 0, true
+		}
+		left, leftBalanced := dfs(root.Left)
+		right, rightBalanced := dfs(root.Right)
+		
+		isBalanced := leftBalanced && rightBalanced && abs(left, right) <= 1
+
+		return 1 + max(left, right), isBalanced
+	}
+	_, ok := dfs(root)
+	return ok
+}
+
+func abs(a int, b int) int {
+	if a > b {
+		return a - b
+	}
+	return b - a
+}
