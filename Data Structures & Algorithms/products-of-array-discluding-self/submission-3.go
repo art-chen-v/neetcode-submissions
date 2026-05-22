2c72587e08d9ef8f96ac5bf953d31fc5b9585ed7
Artem Chen <art.chen.v@gmail.com>
2026-05-22 14:22:33 +0900

    Add: products-of-array-discluding-self - submission-3

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-3.go
@@ -0,0 +1,14 @@
+func productExceptSelf(nums []int) []int {
+	output := make([]int, len(nums))
+	prefix := 1
+	for i, n := range nums {
+		output[i] = prefix
+		prefix *= n
+	}
+	postfix := 1
+	for i := len(nums) - 1; i >= 0; i-- {
+		output[i] *= postfix
+		postfix *= nums[i]
+	}
+	return output
+}
