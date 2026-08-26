47f16ff31ddd1bcce339d13ab8638cd180b78b14
Artem Chen <art.chen.v@gmail.com>
2026-08-27 00:57:04 +0900

    Add: top-k-elements-in-list - submission-14

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-14.go
@@ -0,0 +1,21 @@
+func topKFrequent(nums []int, k int) []int {
+	freq := make(map[int]int)
+	bucket := make([][]int, len(nums) + 1)
+
+	for _, n := range nums {
+		freq[n]++
+	}
+	for k, v := range freq {
+		bucket[v] = append(bucket[v], k)
+	}
+	res := make([]int, 0)
+	for i := len(bucket) - 1; i > 0; i-- {
+		for _, n := range bucket[i] {
+			if k > 0 {
+				res = append(res, n)
+				k--
+			}
+		}
+	}
+	return res
+}
